3e4ba7b03d0f8d9aa775f70e6808e06a6fc9f8d0
jeremien <jeremie.nuel@gmail.com>
2018-03-22 18:48:09 +0100

    changement du titre au scroll

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
-
 $(document).ready(function() {
+
   var header = $(".header")
 
   $(window).on('scroll', function() {
     var scroll = $(window).scrollTop()
     var height = header.height()
-    console.log(scroll, height)
 
-      if (scroll >= height) {
-        console.log('fade out')
-        header.fadeOut("slow")
-      } else {
-        console.log('fade in')
-        header.fadeIn("slow")
-      }
-      //
+    if (scroll >= height + 50) {
+      header.fadeOut("slow")
+
+    } else if (scroll === 0) {
+      header.fadeIn("slow")
+    }
+
+    changeTitre("#titre1", "change1", scroll)
+    changeTitre("#titre2", "change2", scroll)
+    changeTitre("#titre3", "change3", scroll)
+
 
   })
+
+  function changeTitre(tag, titre, scroll) {
+    var t = $(tag)
+    var p = t.position()
+    if (p.top < scroll + 50) {
+          t.text(titre)
+    } else {
+          t.text("no titre")
+    }
+  }
 })
